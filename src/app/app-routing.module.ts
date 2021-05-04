6593421c71ec2e27789335c9b44ed0b137c855af
jdlcgarcia <jcruzgar@gmail.com>
2021-05-04 11:20:05 +0200

    Componentes de UI
    esqueleto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './ui/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      // Here we will add our application pages
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
