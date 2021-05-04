55ea9c0853c278ed3f8e4116a53283a3c98d01b1
jdlcgarcia <jcruzgar@gmail.com>
2021-05-04 12:58:43 +0200

    creación y uso de servicio

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
-
+import { EInfoComponent } from '../../e-info/e-info.component';
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    EInfoComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
